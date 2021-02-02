d21e0e5fbbba894ac2b30f8447d2b53d95dff01d
martin maguire <martin.maguire89@hotmail.co.uk>
2021-02-02 16:50:22 +0000

    scroll effect added for left & right

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-const boxes = document.querySelectorAll('.box')
+  const boxes = document.querySelectorAll('.box')
+  const boxes2 = document.querySelectorAll('.box2')
 
 window.addEventListener('scroll', checkBoxes)
 
@@ -8,12 +9,21 @@ function checkBoxes() {
     const triggerBottom = window.innerHeight / 5 * 4
 
     boxes.forEach(box => {
-        const boxTop = box. getBoundingClientRect().top
+        const boxTop = box.getBoundingClientRect().top
 
         if(boxTop < triggerBottom) {
             box.classList.add('show')
         } else {
             box.classList.remove('show')
-        }       
+        }
+    })
+    boxes2.forEach(box2 => {
+        const boxTop = box2.getBoundingClientRect().top
+
+        if(boxTop < triggerBottom) {
+            box2.classList.add('show')
+        } else {
+            box2.classList.remove('show')
+        }
     })
 }
